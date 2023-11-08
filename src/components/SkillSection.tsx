99943c96aa84c779dd18d0f6eff8eee9445017cd
Paul Su <su.paul.cy@gmail.com>
2023-11-07 23:27:33 -0800

    style skill section

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -1,18 +1,45 @@
+import "../assets/styles/skill.styles.css";
+import ReactIcon from "../assets/images/react.svg"
+import PhpIcon from "../assets/images/php.svg"
+
+const skills = [
+  {
+    title: 'Frontend Dev',
+    icon: ReactIcon,
+    alt: "React Logo",
+    description: 'React, Typescript',
+    experience: '3+ years of experience',
+    color: 'green'
+  },
+  {
+    title: 'Fullstack Dev',
+    icon: PhpIcon,
+    alt: "PHP Logo",
+    description: 'Laravel, PostgreSQL',
+    experience: '2+ years of experience',
+    color: 'yellow'
+  }
+]
+
 const SkillSection = () => {
   return (
     <div id="skill">
-      <h2>Skills</h2>
-      <div style={{ display: 'flex' }}>
-        <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
-          <img src="path/to/react-logo.png" alt="React Logo" style={{ height: '50px', width: '50px' }} />
-          <h3>Frontend Development</h3>
-          <p>Description of Frontend Development skill</p>
-        </div>
-        {/* <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
-          <img src="path/to/php-logo.png" alt="PHP Logo" style={{ height: '50px', width: '50px' }} />
-          <h3>Fullstack Development</h3>
-          <p>Description of Fullstack Development skill</p>
-        </div> */}
+      <h2 className={"text-center"}>Skills</h2>
+      <div className={"skills-container"}>
+        {skills.map((skill, index) => (
+          <div className="skill" key={'skill-' + index}>
+            <div className="skill-title-container">
+              <img src={skill.icon} alt={skill.alt} className="icon" />
+              <div>
+                <h3 className={"underline-" + skill.color + " skill-title"}>{skill.title}</h3>
+                <h3 className="skill-description">{skill.description}</h3>
+              </div>
+            </div>
+            <div className="skill-content">
+              <p className="skill-experience">{skill.experience}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
