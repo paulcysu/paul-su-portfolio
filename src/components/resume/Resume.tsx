4298d39f1daf052bd81c909f3ee37f3b43107b3a
Paul Su <su.paul.cy@gmail.com>
2023-11-17 16:24:54 -0800

    fix: background img->webp and accordian bug from uuid

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import "./resume.styles.css";
 import { ExperienceType } from "../../data/experiences";
 import Experience from "./Experience";
-import { v4 as uuidv4 } from 'uuid';
 
 const Resume: React.FC<{ list: ExperienceType[] }> = ({ list }) => {
   const [expanded, setExpanded] = React.useState<string | false>("e1");
@@ -16,7 +15,7 @@ const Resume: React.FC<{ list: ExperienceType[] }> = ({ list }) => {
     <div className="experience-container">
       {list.map((experience) => (
         <Experience
-          key={uuidv4()}
+          key={experience.id}
           experience={experience}
           expanded={expanded === experience.id}
           onClick={handleChange}
